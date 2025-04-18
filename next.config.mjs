1ec9a13846c9927d2a95f2e574237eff494f60a2
shapefuture <159969400+shapefuture@users.noreply.github.com>
2025-04-18 16:31:01 +0000

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+import withPWA from 'next-pwa';
 import path from 'path';
 
 const nextConfig = {
@@ -13,12 +14,12 @@ const nextConfig = {
   },
 };
 
-export default {
-  output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/Aeon' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Aeon/' : '',
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.join(import.meta.dirname, '');
-    return config;
+export default withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
   },
-};
+});
